f1d9838927c92710fdd53cc3bf19cd682a0e409e
Suyash Pathak <suyashp271@gmail.com>
2025-08-07 20:36:31 +0530

    Update signup.jsx

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -15,7 +15,7 @@ const Signup = () => {
     e.preventDefault();
     setLoading(true);
     try {
-      const res = await fetch('http://localhost:5000/api/auth/signup', {
+      const res = await fetch('https://link-saver-qk9d.onrender.com/api/auth/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
